15c055b0d18ad40ce0a8884e89db2b2db34b560e
David C <45155292+DavoDC@users.noreply.github.com>
2022-05-12 12:21:20 +0800

    Minor improvements
    cleaned up DC COMMands cits3003 projct and added glew issue reminder
    
    added gitignore comment

--- CITS3003 - Graphics/Project - Scene Creator/DC_COMMANDS.sh
+++ CITS3003 - Graphics/Project - Scene Creator/DC_COMMANDS.sh
@@ -1,23 +1,31 @@
 
-# Dependencies
-# sudo apt install cmake libxmu-dev g++ libx11-dev libgl1-mesa-dev libglu1-mesa-dev xorg-dev
 
-# Clean before (only if needed)
-# cmake --build cmake-build --target clean
-
-
-# If build is broken:
-# 1) Delete 'cmake-build' folder
-# 2) Re-generate build files
+### COMMANDS FOR CITS3003 PROJECT
 
 
 # Generate build files (only done once at the start)
-# cmake -S . -B cmake-build
+# If build is broken:
+# 1) Delete 'cmake-build' folder
+# 2) Re-generate build files by running this again
+cmake -S . -B cmake-build
 
 
 # Build project (do when .cpp file has been edited)
-cmake --build cmake-build
+#cmake --build cmake-build
 
 # Run project
-./start_scene
+#./start_scene
 
+
+### NOTES
+
+# For dependencies, run GEN SETUP from Linux Files!
+
+# If you get this error:
+# >>> CMake Error at lib/glew/CMakeLists.txt:94 (add_library):
+# >>>   Cannot find source file:
+# >>>     /home/david/GitHub_Repos/UWA-CompSci
+# >>>     /CITS3003 - Graphics/Project - Scene Creator/lib/glew/src/glew.c
+# It means that something went wrong with the repo,
+# and needed files are missing.
+# Try recloning or getting the project template files.
