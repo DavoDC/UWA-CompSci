3c88bf1180217c14c7810458e15d68d44ec7108c
David C <45155292+DavoDC@users.noreply.github.com>
2022-05-12 13:22:25 +0800

    CITS3003 Project - DC COMMANDs - restored
    restored back to normal usage
    
    added removing of assimp log

--- CITS3003 - Graphics/Project - Scene Creator/DC_COMMANDS.sh
+++ CITS3003 - Graphics/Project - Scene Creator/DC_COMMANDS.sh
@@ -7,14 +7,18 @@
 # If build is broken:
 # 1) Delete 'cmake-build' folder
 # 2) Re-generate build files by running this again
-cmake -S . -B cmake-build
+### cmake -S . -B cmake-build
 
 
 # Build project (do when .cpp file has been edited)
-#cmake --build cmake-build
+cmake --build cmake-build
 
 # Run project
-#./start_scene
+./start_scene
+
+
+# Remove assimp log
+rm assimp_log.txt
 
 
 ### NOTES
